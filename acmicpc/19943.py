2c687e2f521f6448e77b39126dc7d4416d40a4d9
lavi02 <hoil.jeong@fleetune.com>
2023-08-07 01:39:50 +0900

    1

--- acmicpc/19943.py
+++ acmicpc/19943.py
@@ -1,6 +1,7 @@
 import sys
 from collections import deque
 
+result = []
 class ConvexHull:
     def __init__(self):
         self.la = [0] * 101001
@@ -29,57 +30,57 @@ class ConvexHull:
                 hi = mid
         return self.la[lo] * x + self.lb[lo]
 
+mod = [".00", ".25", ".50", ".75"]
+for _ in range(int(sys.stdin.readline())):
+    n = int(sys.stdin.readline())
+    px, py = [], []
+    for _ in range(n):
+        x, y = map(int, sys.stdin.readline().split())
+        px.append(x)
+        py.append(y)
 
-def solve():
-    mod = [".00", ".25", ".50", ".75"]
-    for _ in range(int(sys.stdin.readline())):
-        n = int(sys.stdin.readline())
-        px, py = [], []
-        for _ in range(n):
-            x, y = map(int, sys.stdin.readline().split())
-            px.append(x)
-            py.append(y)
+    stk = deque()
+    stk.append(0)
 
-        stk = deque()
-        stk.append(0)
+    for next in range(1, n):
+        now = stk[-1]
+        dx = (px[now] + px[next] - py[now] + py[next]) / 2.0
+        if dx >= px[next]:
+            while dx >= px[next]:
+                stk.pop()
+                if len(stk) == 0:
+                    break
+                now = stk[-1]
+                dx = (px[now] + px[next] - py[now] + py[next]) / 2.0
+            stk.append(next)
+        elif dx <= px[now]:
+            pass
+        else:
+            stk.append(next)
 
-        for next in range(1, n):
-            now = stk[-1]
-            dx = (px[now] + px[next] - py[now] + py[next]) / 2.0
-            if dx >= px[next]:
-                while dx >= px[next]:
-                    stk.pop()
-                    if len(stk) == 0:
-                        break
-                    now = stk[-1]
-                    dx = (px[now] + px[next] - py[now] + py[next]) / 2.0
-                stk.append(next)
-            elif dx <= px[now]:
-                pass
-            else:
-                stk.append(next)
+    n = len(stk)
+    lx, ly = [], []
 
-        n = len(stk)
-        lx, ly = [], []
+    while len(stk) != 0:
+        top = stk.pop()
+        lx.append(px[top])
+        ly.append(py[top])
 
-        while len(stk) != 0:
-            top = stk.pop()
-            lx.append(px[top])
-            ly.append(py[top])
+    lx.reverse()
+    ly.reverse()
 
-        lx.reverse()
-        ly.reverse()
+    hull = ConvexHull()
+    dp = [0] * (n + 1)
+    hull.insert(-2 * (lx[0] - ly[0]), (lx[0] - ly[0]) * (lx[0] - ly[0]))
+    for i in range(n):
+        u = lx[i] + ly[i]
+        v = lx[i + 1] - ly[i + 1] if i + 1 < n else 0
+        dp[i] = hull.query(u) + u * u
+        hull.insert(-2 * v, v * v + dp[i])
 
-        hull = ConvexHull()
-        dp = [0] * (n + 1)
-        hull.insert(-2 * (lx[0] - ly[0]), (lx[0] - ly[0]) * (lx[0] - ly[0]))
-        for i in range(n):
-            u = lx[i] + ly[i]
-            v = lx[i + 1] - ly[i + 1] if i + 1 < n else 0
-            dp[i] = hull.query(u) + u * u
-            hull.insert(-2 * v, v * v + dp[i])
+    data = str(dp[n - 1] // 4)+mod[dp[n - 1] % 4]
+    result.append(data)
 
-        print(dp[n - 1] // 4, mod[dp[n - 1] % 4])
-
-solve()
+for i in result:
+    print(i)
 
